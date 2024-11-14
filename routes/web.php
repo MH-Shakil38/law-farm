ad30e8a56fb27438eb6b4e909cc3a4e8f9a3c541
MH-Shakil38 <maynuddinhsn@gmail.com>
2024-11-14 22:01:20 +0600

    ..

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::get('/', function () {
     return view('admin.dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
