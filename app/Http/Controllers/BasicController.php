1ac00bbce392562fdce4adc19eb944a5088a3e2d
MH-Shakil38 <maynuddinhsn@gmail.com>
2025-01-07 16:22:13 +0600

    ..

--- app/Http/Controllers/BasicController.php
+++ app/Http/Controllers/BasicController.php
@@ -7,6 +7,7 @@ use App\Models\CaseType;
 use App\Models\Client;
 use App\Models\User;
 use App\Services\ActivityLogService;
+use App\Services\ClientService;
 use App\Services\MailService;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
@@ -20,30 +21,30 @@ class BasicController extends Controller
     {
         return view('website.website');
     }
-    public function dashboard()
+    public function dashboard(ClientService $clientService)
     {
-        $response = Http::post('https://api.verizon.com/login', [
-            'email' => 'pemalaw5@gmail.com',
-            'password' => 'BTenzingL2024@',
-        ]);
-        if ($response->successful()) {
-            $data = $response->json();
-            $token = $data['access_token'] ?? null;
-            dd($token);
-            if ($token) {
-                dd($token);
-                echo "Token retrieved: $token";
-            } else {
-                echo "Failed to get a token.";
-            }
-        } else {
-            echo "Authentication failed. Check credentials.";
-        }
+        // $response = Http::post('https://api.verizon.com/login', [
+        //     'email' => 'pemalaw5@gmail.com',
+        //     'password' => 'BTenzingL2024@',
+        // ]);
+        // if ($response->successful()) {
+        //     $data = $response->json();
+        //     $token = $data['access_token'] ?? null;
+        //     dd($token);
+        //     if ($token) {
+        //         dd($token);
+        //         echo "Token retrieved: $token";
+        //     } else {
+        //         echo "Failed to get a token.";
+        //     }
+        // } else {
+        //     echo "Authentication failed. Check credentials.";
+        // }
         $data['todayClient'] = Client::query()->whereDate('created_at',today())->get();
         $data['todayCase'] = Client::query()->whereDate('hearing_date',today())->get();
         $data['tomorrowCase'] = Client::query()->whereDate('hearing_date',Carbon::tomorrow()->toDateString())->get();
         $data['caseTypes'] = CaseType::getAll();
-        $data['clients'] = Client::getAll(true);
+        $data['clients'] = $clientService->getAll(true);
         $data['onlineUsers'] = User::query()->get();
         ActivityLogService::LogInfo('Dashboard');
         return view('admin.dashboard')->with($data);
